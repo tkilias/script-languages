6f3e48d4febcc57c10a5c516e08dbc7bcb218aa8
Torsten Kilias <torsten.kilias@exasol.com>
2019-08-06 18:21:37 +0200

    Add timestamp to .build_output upload and fix decrypt.sh call

--- google-cloud-build/check_build.sh
+++ google-cloud-build/check_build.sh
@@ -5,7 +5,13 @@ set -o pipefail
 LOG_BUCKET="$1"
 FLAVOR="$2"
 BUILD_ID="$3"
-gsutil rsync -x exports -r .build_output "$LOG_BUCKET/build_output/$FLAVOR/$BUILD_ID/"
+DATETIME_FILE="datetime.txt"
+if [ -f "$DATETIME_FILE" ]
+then
+	date --utc +%Y%m%d_%H%M%S > "$DATETIME_FILE"
+fi
+DATETIME=$(cat $DATETIME_FILE)
+gsutil rsync -x exports -r .build_output "$LOG_BUCKET/build_output/$FLAVOR/${DATETIME}_${BUILD_ID}/"
 if [[ $(< /workspace/build-status.txt) == "fail" ]]; then
 	exit 1
 fi
