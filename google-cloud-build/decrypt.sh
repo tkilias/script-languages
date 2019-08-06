de42aba03ecc18a6bb32eeaaa5eb3d38e8505091
Torsten Kilias <torsten.kilias@exasol.com>
2019-08-06 20:04:09 +0200

    Fix decrypt.sh

--- google-cloud-build/decrypt.sh
+++ google-cloud-build/decrypt.sh
@@ -4,10 +4,10 @@ set -o nounset
 set -o pipefail
 ENCRYPTED_SECRET=$1
 SECRET_NAME=$2
-KEYRING_NAME=$3
+KEY_RING_NAME=$3
 KEY_NAME=$4
 mkdir -p secrets
 echo -n "$ENCRYPTED_SECRET" |
 	base64 -d -w |
-       	gcloud kms decrypt --plaintext-file=- --ciphertext-file=- --location=global --keyring=$KEYRING_NAME --key=$KEY_NAME > "secrets/$SECRET_NAME"
+       	gcloud kms decrypt --plaintext-file=- --ciphertext-file=- --location=global --keyring=$KEY_RING_NAME --key=$KEY_NAME > "secrets/$SECRET_NAME"
 cat "secrets/$SECRET_NAME"
