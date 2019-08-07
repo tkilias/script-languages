525b55301ac0b170ac0500f85662eaf46c545874
Torsten Kilias <torsten.kilias@exasol.com>
2019-08-07 18:17:27 +0200

    Add more logging to push to find the bug

--- google-cloud-build/ci-scripts/push_to_build_cache.sh
+++ google-cloud-build/ci-scripts/push_to_build_cache.sh
@@ -10,5 +10,4 @@ export TARGET_DOCKER_PASSWORD=$(cat secrets/DOCKER_PASSWORD)
 TARGET_TAG_PREFIX="$COMMIT"
 TARGET_OPTIONS="--target-docker-repository-name '$DOCKER_REPOSITORY' --target-docker-username '$DOCKER_USERNAME' --target-docker-tag-prefix '$TARGET_TAG_PREFIX'"
 touch /workspace/build-status.txtdd
-docker images
 ./exaslct push $TARGET_OPTIONS --push-all --force-push --flavor-path "flavors/$FLAVOR" --workers 7 || echo "fail" > /workspace/build-status.txt
