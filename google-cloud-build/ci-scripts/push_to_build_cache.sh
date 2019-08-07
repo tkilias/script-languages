50d1565bb2e28fedb76d3ed1e893acf40d3f3f7d
Torsten Kilias <torsten.kilias@exasol.com>
2019-08-07 14:58:54 +0200

    Add push_to_build_cache

---
+++ google-cloud-build/ci-scripts/push_to_build_cache.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -o errexit
+set -o nounset
+FLAVOR=$1
+DOCKER_REPOSITORY=$2
+DOCKER_USERNAME=$3
+COMMIT=$4
+TRAGET_DOCKER_PASSWORD=$(cat secrets/DOCKER_PASSWORD)
+TARGET_TAG_PREFIX="$COMMIT"
+TARGET_OPTIONS="--target-docker-repository-name '$DOCKER_REPOSITORY' --target-docker-username '$DOCKER_USERNAME' --target-docker-tag-prefix '$TARGET_TAG_PREFIX'"
+touch /workspace/build-status.txtdd
+./exaslct push $TARGET_OPTIONS --push-all --force-push --flavor-path "flavors/$FLAVOR" --workers 7 || echo "fail" > /workspace/build-status.txt
