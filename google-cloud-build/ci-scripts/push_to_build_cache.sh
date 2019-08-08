e791ee1c2d08a734fd5d990495e0ee98ac560c3c
Torsten Kilias <torsten.kilias@exasol.com>
2019-08-08 12:36:15 +0200

    print parameter in push_to_build_cache.sh for debugging

--- google-cloud-build/ci-scripts/push_to_build_cache.sh
+++ google-cloud-build/ci-scripts/push_to_build_cache.sh
@@ -13,5 +13,17 @@ export TARGET_DOCKER_PASSWORD="$(cat secrets/DOCKER_PASSWORD)"
 SCRIPT_DIR="$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")"
 TARGET_OPTIONS="$($SCRIPT_DIR/docker_options.sh TARGET $TARGET_DOCKER_REPOSITORY $DOCKER_USERNAME $TARGET_TAG_PREFIX)"
 SOURCE_OPTIONS="$($SCRIPT_DIR/docker_options.sh SOURCE $SOURCE_DOCKER_REPOSITORY $DOCKER_USERNAME $SOURCE_TAG_PREFIX)"
-touch /workspace/build-status.txtdd
-./exaslct push $SOURCE_OPTIONS $TARGET_OPTIONS --push-all --force-push --flavor-path "flavors/$FLAVOR" --workers 7 || echo "fail" > /workspace/build-status.txt
+
+echo "FLAVOR: $FLAVOR"
+echo "SOURCE_DOCKER_REPOSITORY: $SOURCE_DOCKER_REPOSITORY"
+echo "SOURCE_TAG_PREFIX: $SOURCE_TAG_PREFIX"
+echo "TARGET_DOCKER_REPOSITORY: $TARGET_DOCKER_REPOSITORY"
+echo "TARGET_TAG_PREFIX: $TARGET_TAG_PREFIX"
+echo "DOCKER_USERNAME: $DOCKER_USERNAME"
+echo "TARGET_OPTIONS: $TARGET_OPTIONS"
+echo "SOURCE_OPTIONS: $SOURCE_OPTIONS"
+
+touch /workspace/build-status.txt
+COMMAND="./exaslct push $SOURCE_OPTIONS $TARGET_OPTIONS --push-all --force-push --flavor-path 'flavors/$FLAVOR' --workers 7"
+echo "Executing Command: $COMMAND"
+bash -c "$COMMAND" || echo "fail" > /workspace/build-status.txt
