0654515337c2d2619f2208af3e491cda8327c4cb
Torsten Kilias <torsten.kilias@exasol.com>
2019-08-08 14:29:24 +0200

    Fix docker_options.sh

--- google-cloud-build/ci-scripts/docker_options.sh
+++ google-cloud-build/ci-scripts/docker_options.sh
@@ -7,15 +7,15 @@ DOCKER_REPOSITORY=$2
 DOCKER_USERNAME=$3
 TAG_PREFIX=$4
 DOCKER_OPTIONS=""
-if [ -n "$DOCKER_REPOSITORY" ] || [ "$DOCKER_REPOSITORY" != '""' ]
+if [ -n "$DOCKER_REPOSITORY" ] && [ "$DOCKER_REPOSITORY" != '""' ]
 then
   DOCKER_OPTIONS="$DOCKER_OPTIONS --$TYPE-docker-repository-name $DOCKER_REPOSITORY"
 fi
-if [ -n "$DOCKER_USERNAME" ] || [ "$DOCKER_USERNAME" != '""' ]
+if [ -n "$DOCKER_USERNAME" ] && [ "$DOCKER_USERNAME" != '""' ]
 then
   DOCKER_OPTIONS="$DOCKER_OPTIONS --$TYPE-docker-username $DOCKER_USERNAME"
 fi
-if [ -n "$TAG_PREFIX" ] || [ "$TAG_PREFIX" != '""' ]
+if [ -n "$TAG_PREFIX" ] && [ "$TAG_PREFIX" != '""' ]
 then
   DOCKER_OPTIONS="$DOCKER_OPTIONS --$TYPE-docker-tag-prefix $TAG_PREFIX"
 fi
