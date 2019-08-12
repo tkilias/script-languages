a86feb99c1011fbc2551be171903c4398d6c6e93
Torsten Kilias <torsten.kilias@exasol.com>
2019-08-12 17:21:35 +0200

    Add github status notification

--- google-cloud-build/setup-scripts/create_encryption_key.sh
+++ google-cloud-build/setup-scripts/create_encryption_key.sh
@@ -7,4 +7,10 @@ gcloud kms keys create $KEY_NAME --location global --keyring $KEY_RING_NAME --pu
 
 PROJECT=$(gcloud config get-value project)
 PROJECT_NUMBER=$(gcloud projects list --filter="$PROJECT" --format="value(PROJECT_NUMBER)")
+
+#Grant key to Cloud Build
 gcloud kms keys add-iam-policy-binding "$KEY_NAME" --location=global --keyring="$KEY_RING_NAME" --member=serviceAccount:$PROJECT_NUMBER@cloudbuild.gserviceaccount.com --role=roles/cloudkms.cryptoKeyDecrypter
+
+#Grant key to Cloud Functions
+gcloud beta iam service-accounts create build-cloud-functions --display-name "build-cloud-functions"
+gcloud kms keys add-iam-policy-binding "$KEY_NAME" --location=global --keyring="$KEY_RING_NAME" --member=serviceAccount:build-cloud-functions@tpu-integration.iam.gserviceaccount.com  --role=roles/cloudkms.cryptoKeyDecrypter
