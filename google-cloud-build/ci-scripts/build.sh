091d8faaa030c19589953ed1e0b026bf31daf234
Torsten Kilias <torsten.kilias@exasol.com>
2019-08-13 15:38:36 +0200

    Add rebuild triggers

--- google-cloud-build/ci-scripts/build.sh
+++ google-cloud-build/ci-scripts/build.sh
@@ -2,6 +2,12 @@
 set -o errexit
 set -o nounset
 set -o pipefail
+REBUILD=$2
 FLAVOR=$1
+ADDITIONAL_ARGUMENTS=""
+if [ "$REBUILD" == "true" ]
+then
+  ADDITIONAL_ARGUMENTS="--force-rebuild"
+fi
 touch /workspace/build-status.txt
-./exaslct build --flavor-path "flavors/$FLAVOR"  --workers 7 || echo "fail" > /workspace/build-status.txt
+./exaslct build --flavor-path "flavors/$FLAVOR" --workers 7 $ADDITIONAL_ARGUMENTS || echo "fail" > /workspace/build-status.txt
--- google-cloud-build/setup-scripts/update_triggers.sh
+++ google-cloud-build/setup-scripts/update_triggers.sh
@@ -4,48 +4,53 @@ set -o nounset
 set -o pipefail
 
 function generate_build_json(){
-	cat "$env_file" "$encrypted_docker_password_file" "$encrypted_github_token_file" $* > data.yaml
-  echo "job_id: \"${I##*$triggers/flavor-config/}\""  >> data.yaml
-	trigger_file=$(cat "$I" | yq -r .trigger_template_file)
-	jinja2 $triggers/$trigger_file data.yaml > build.json
+	cat "$ENV_FILE" "$ENCRYPTED_DOCKER_PASSWORD_FILE" "$ENCRYPTED_GITHUB_TOKEN_FILE" $* > data.yaml
+  echo "job_id: \"${TRIGGER_CONFIG_PATH##*$TRIGGERS/flavor-config/}\""  >> data.yaml
+	TRIGGER_FILE=$(cat "$TRIGGER_CONFIG_PATH" | yq -r .trigger_template_file)
+	jinja2 $TRIGGERS/$TRIGGER_FILE data.yaml > build.json
 	rm data.yaml
 }
 
 function create(){
-	echo "creating" $I
-	generate_build_json "$I"
-	create_output=$($setup_scripts/create_build_trigger.sh build.json)
+	echo "creating" $TRIGGER_CONFIG_PATH
+	generate_build_json "$TRIGGER_CONFIG_PATH"
+	CREATE_OUTPUT=$($SETUP_SCRIPTS/create_build_trigger.sh build.json)
 	rm build.json
-	echo "$create_output"
-	trigger_id=$(echo "$create_output" | jq .id)
-	mkdir -p $(dirname "$env_flavor_config_path")
-	echo "trigger_id: $trigger_id"  > "$env_flavor_config_path"
+	echo "$CREATE_OUTPUT"
+	TRIGGER_ID=$(echo "$CREATE_OUTPUT" | jq .id)
+	mkdir -p $(dirname "$ENV_FLAVOR_CONFIG_PATH")
+	echo "trigger_id: $TRIGGER_ID"  > "$ENV_FLAVOR_CONFIG_PATH"
 }
 
 function update(){
-	echo "updating" $I
-	generate_build_json "$env_flavor_config_path" "$I"
-	$setup_scripts/update_build_trigger.sh build.json
+	echo "updating" $TRIGGER_CONFIG_PATH
+	generate_build_json "$ENV_FLAVOR_CONFIG_PATH" "$TRIGGER_CONFIG_PATH"
+	$SETUP_SCRIPTS/update_build_trigger.sh build.json
 	rm build.json
 }
 
+function create_or_update(){
+  ENV_FLAVOR_CONFIG_PATH=".env/$TRIGGER_CONFIG_PATH"
+  if [ -f "$ENV_FLAVOR_CONFIG_PATH" ]
+  then
+    update
+  else
+    create
+  fi
+}
+
+
 function main(){
-	setup_scripts=setup-scripts
-	triggers=triggers
-	$setup_scripts/create_encrypted_docker_password.sh
-	$setup_scripts/create_encrypted_github_token.sh
-	env_file=".env/env.yaml"
-	encrypted_docker_password_file=".env/encrypted_docker_password.yaml"
-	encrypted_github_token_file=".env/encrypted_github_token.yaml"
-	for I in $(find $triggers/flavor-config -name '*.yaml')
-	do
-		env_flavor_config_path=".env/$I"
-		if [ -f "$env_flavor_config_path" ]
-		then
-			update
-		else
-			create
-		fi
+	SETUP_SCRIPTS=setup-scripts
+	TRIGGERS=triggers
+	$SETUP_SCRIPTS/create_encrypted_docker_password.sh
+	$SETUP_SCRIPTS/create_encrypted_github_token.sh
+	ENV_FILE=".env/env.yaml"
+	ENCRYPTED_DOCKER_PASSWORD_FILE=".env/encrypted_docker_password.yaml"
+	ENCRYPTED_GITHUB_TOKEN_FILE=".env/encrypted_github_token.yaml"
+	for TRIGGER_CONFIG_PATH in $(find $TRIGGERS/flavor-config -name '*.yaml')
+  do
+	  create_or_update
 	done
 }
 
