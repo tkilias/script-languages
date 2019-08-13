08476baf7ae1b3a2d7cdfa804c27816fb22c45d7
Torsten Kilias <torsten.kilias@exasol.com>
2019-08-13 16:56:45 +0200

    ci-scripts/build.sh print command

--- google-cloud-build/ci-scripts/build.sh
+++ google-cloud-build/ci-scripts/build.sh
@@ -10,4 +10,6 @@ then
   ADDITIONAL_ARGUMENTS="--force-rebuild"
 fi
 touch /workspace/build-status.txt
-./exaslct build --flavor-path "flavors/$FLAVOR" --workers 7 $ADDITIONAL_ARGUMENTS || echo "fail" > /workspace/build-status.txt
+COMMAND="./exaslct build --flavor-path "flavors/$FLAVOR" --workers 7 $ADDITIONAL_ARGUMENTS"
+echo "Executing Command: $COMMAND"
+$COMMAND || echo "fail" > /workspace/build-status.txt
